9341dade41884683e052480fb02d4774aebe554a
Sune Engtorp <suneengtorp@live.dk>
2023-04-16 21:48:54 +0200

    added hardcoded emotes

---
+++ AndersBot/Emotes.cs
@@ -0,0 +1,34 @@
+﻿namespace AndersBot
+{
+    public static class Mentorhold8
+    {
+        public static string
+            HYPERS = "<:HYPERS:1034153332316577864>",
+            poggers = "<:poggers:913150793375416431>",
+            sadge = "<:sadge:785492435043876865>",
+            hmmNice = "<:hmmNice:1034181340192575658>",
+            LETSGO = "<:LETSGO:1034180674019663882>",
+            NOOOO = "<:NOOOO:1034183132854878308>",
+            peepoLove = "<:peepoLove:1034181190602735697>",
+            monkaS = "<:monkaS:903248816701054978>",
+            flushed_sunglasses = "<:flushed_sunglasses:1082405768789229740>",
+            nerd = "<:nerd:1082406285565243392>",
+            monkaGun = "<:monkaGun:1034180454099734548>",
+            sune_angery = "<:sune_angery:1063559065336299540>",
+            pepejam = "<:pepejam:763330791106412544>",
+            wut = "<:wut:1063559503322288208>",
+            illdrinktothat = "<:illdrinktothat:1060212918328758302>",
+            peepoShy = "<:peepoShy:1034142460009525332>",
+            sadpepejuice = "<:sadpepejuice:756092314375684147>",
+            peepoDown = "<:peepoDown:1034183157118931054>",
+            peepoEvil = "<:peepoEvil:1086234566513131580>",
+            peepoSit = "<:peepoSit:1034183143894290523>",
+            peepoPoo = "<:peepoPoo:1034153732079882240>",
+            notlikethis = "<:notlikethis:811603592897232926>",
+            isforme = "<:isforme:785500595107659777>",
+            cowboythink = "<:cowboythink:755731824252026901>",
+            madge = "<:Madge:1034154157256474706>",
+            a_hackermans = "<a:HACKERMANS:587667462670123060>";
+
+    }
+}
